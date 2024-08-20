7c36c4481b338ab416b09aff6267cba45d6b406d
prasannan-robots <prasannanatarajan3595@gmail.com>
2024-08-21 05:00:30 +0530

    added source des

---
+++ lib/map.dart
@@ -0,0 +1,226 @@
+import 'package:charger/planner.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/place_type.dart';
+import 'package:google_places_flutter/model/prediction.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final TextEditingController _sourceController = TextEditingController();
+  final TextEditingController _destinationController = TextEditingController();
+  final TextEditingController _rangeController = TextEditingController();
+  Set<Marker> _markers = {};
+  Set<Polyline> _polylines = {};
+  bool _isLoading = false;
+  final GroundData _map = GroundData();
+  LatLng _currentPosition = const LatLng(
+      13.085758559399656, 80.1754404576725); // Example start location
+// 11.387819148378416, 79.73058865396625
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Position position = await _map.determinePosition();
+    setState(() {
+      _currentPosition = LatLng(position.latitude, position.longitude);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Charger'),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: _currentPosition,
+              zoom: 12,
+            ),
+            markers: _markers,
+            polylines: _polylines,
+            onMapCreated: (GoogleMapController controller) {},
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        GooglePlaceAutoCompleteTextField(
+                          textEditingController: _sourceController,
+                          googleAPIKey:
+                              "AIzaSyD-tezFTBpKRVH1icGYGyJUP4SQPyUrPaE",
+                          inputDecoration: const InputDecoration(
+                              label: Text('Enter Source')),
+                          debounceTime: 800, // default 600 ms,
+                          countries: const [
+                            "in",
+                            "fr"
+                          ], // optional by default null is set
+                          isLatLngRequired:
+                              true, // if you required coordinates from place detail
+                          getPlaceDetailWithLatLng: (Prediction prediction) {
+                            // this method will return latlng with place detail
+                            print("placeDetails" + prediction.lng.toString());
+                            _sourceController.text =
+                                '${prediction.lat.toString()},${prediction.lng.toString()}';
+                          }, // this callback is called when isLatLngRequired is true
+                          itemClick: (Prediction prediction) {},
+                          // if we want to make custom list item builder
+                          itemBuilder: (context, index, Prediction prediction) {
+                            return Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.location_on),
+                                  const SizedBox(
+                                    width: 7,
+                                  ),
+                                  Expanded(
+                                      child: Text(
+                                          "${prediction.description ?? ""}"))
+                                ],
+                              ),
+                            );
+                          },
+                          // if you want to add seperator between list items
+                          seperatedBuilder: const Divider(),
+                          // want to show close icon
+                          isCrossBtnShown: true,
+                          // optional container padding
+                          containerHorizontalPadding: 10,
+                          // place type
+                          placeType: PlaceType.geocode,
+                        ),
+                        const SizedBox(height: 10),
+                        GooglePlaceAutoCompleteTextField(
+                          textEditingController: _destinationController,
+                          googleAPIKey:
+                              "AIzaSyD-tezFTBpKRVH1icGYGyJUP4SQPyUrPaE",
+                          inputDecoration: const InputDecoration(
+                              label: Text('Enter Destination')),
+                          debounceTime: 800, // default 600 ms,
+                          countries: const [
+                            "in",
+                            "fr"
+                          ], // optional by default null is set
+                          isLatLngRequired:
+                              true, // if you required coordinates from place detail
+                          getPlaceDetailWithLatLng: (Prediction prediction) {
+                            // this method will return latlng with place detail
+                            print("placeDetails" + prediction.lng.toString());
+                            _destinationController.text =
+                                '${prediction.lat.toString()},${prediction.lng.toString()}';
+                          }, // this callback is called when isLatLngRequired is true
+                          itemClick: (Prediction prediction) {
+                            // _destinationController.selection =
+                            //     TextSelection.fromPosition(TextPosition(
+                            //         offset:
+                            //             prediction.description?.length ?? 0));
+                          },
+                          // if we want to make custom list item builder
+                          itemBuilder: (context, index, Prediction prediction) {
+                            return Container(
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                children: [
+                                  const Icon(Icons.location_on),
+                                  const SizedBox(
+                                    width: 7,
+                                  ),
+                                  Expanded(
+                                      child: Text(
+                                          "${prediction.description ?? ""}"))
+                                ],
+                              ),
+                            );
+                          },
+                          // if you want to add seperator between list items
+                          seperatedBuilder: const Divider(),
+                          // want to show close icon
+                          isCrossBtnShown: true,
+                          // optional container padding
+                          containerHorizontalPadding: 10,
+                          // place type
+                          placeType: PlaceType.geocode,
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: _rangeController,
+                          decoration: InputDecoration(
+                            hintText: 'Enter vehicle range',
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ))),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          setState(() {
+            _isLoading = true;
+          });
+          String source = _sourceController.text;
+          String destination = _destinationController.text;
+          double rangekm = double.tryParse(_rangeController.text) ?? 100.0;
+
+          List<LatLng> routeData = await _map.calculateRoute(
+              _parseLatLng(source), _parseLatLng(destination));
+          double rangeKm = _map.estimateRange(40, rangekm);
+
+          Set<Marker> chargingStations =
+              await _map.selectChargingStations(routeData, rangeKm);
+
+          setState(() {
+            _isLoading = false;
+            _markers = chargingStations;
+            _polylines = {
+              Polyline(
+                polylineId: const PolylineId("route"),
+                points: routeData,
+                color: Colors.blue,
+                width: 5,
+              ),
+            };
+          });
+        },
+        child: Icon(_isLoading ? Icons.rocket : Icons.directions),
+      ),
+    );
+  }
+}
+
+LatLng _parseLatLng(String input) {
+  final parts = input.split(',');
+  if (parts.length == 2) {
+    final lat = double.tryParse(parts[0].trim());
+    final lng = double.tryParse(parts[1].trim());
+    if (lat != null && lng != null) {
+      return LatLng(lat, lng);
+    }
+  }
+  return const LatLng(0.0, 0.0);
+}
---
+++ lib/planner.dart
@@ -0,0 +1,239 @@
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GroundData {
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<Set<Marker>> fetchChargingStations(
+      double latitude, double longitude) async {
+    const url = 'https://places.googleapis.com/v1/places:searchNearby';
+    final headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': 'AIzaSyD-tezFTBpKRVH1icGYGyJUP4SQPyUrPaE',
+      'X-Goog-FieldMask': '*',
+    };
+    String body = jsonEncode({
+      'includedTypes': ['electric_vehicle_charging_station'],
+      'maxResultCount': 10,
+      'locationRestriction': {
+        'circle': {
+          'center': {
+            'latitude': latitude,
+            'longitude': longitude,
+          },
+          'radius': 500.0,
+        },
+      },
+    });
+    final response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+    final Map<String, dynamic> jsonResponse = json.decode(response.body);
+    final List<dynamic>? places =
+        jsonResponse.containsKey('places') ? jsonResponse['places'] : null;
+
+    if (places != null) {
+      Set<Marker> markers = places.map((place) {
+        return Marker(
+          markerId: MarkerId(place['displayName']['text']),
+          position: LatLng(
+              place['location']['latitude'], place['location']['longitude']),
+          infoWindow: InfoWindow(title: place['displayName']['text']),
+        );
+      }).toSet();
+      return markers;
+    }
+    return {};
+  }
+
+  Future<Set<Marker>> selectChargingStations(
+      List<LatLng> routePoints, double rangeKm) async {
+    Set<Marker> chargingStations = {};
+    double traveledDistance = 0.0;
+
+    for (int i = 1; i < routePoints.length; i++) {
+      double stepDistance = Geolocator.distanceBetween(
+            routePoints[i - 1].latitude,
+            routePoints[i - 1].longitude,
+            routePoints[i].latitude,
+            routePoints[i].longitude,
+          ) /
+          1000.0; // Convert to km
+      traveledDistance += stepDistance;
+
+      if (traveledDistance >= rangeKm) {
+        LatLng stepLocation = routePoints[i];
+        // Fetch nearby charging stations using the Google Places API
+        print('Checking for charging stations at $stepLocation');
+        Set<Marker> nearbyStations = await fetchChargingStations(
+            stepLocation.latitude, stepLocation.longitude);
+
+        if (nearbyStations.isNotEmpty) {
+          chargingStations
+              .add(nearbyStations.first); // Select the nearest station
+          traveledDistance = 0.0; // Reset distance after a charging stop
+        } else {
+          // Backtrack along the route points to check for chargers
+          LatLng? lastCheckedLocation;
+          print('Backtracking to check for charging stations');
+          for (int j = i - 1; j >= 0; j--) {
+            LatLng backtrackLocation = routePoints[j];
+            if (lastCheckedLocation != null) {
+              double backtrackDistance = Geolocator.distanceBetween(
+                    lastCheckedLocation.latitude,
+                    lastCheckedLocation.longitude,
+                    backtrackLocation.latitude,
+                    backtrackLocation.longitude,
+                  ) /
+                  1000.0; // Convert to km
+              if (backtrackDistance < 1.0) {
+                continue; // Skip points within 500 meters
+              }
+            }
+            lastCheckedLocation = backtrackLocation;
+            print(
+                'BackTrack: Checking for charging stations at ${backtrackLocation.latitude} ${backtrackLocation.longitude} $j');
+
+            Set<Marker> backtrackStations = await fetchChargingStations(
+                backtrackLocation.latitude, backtrackLocation.longitude);
+
+            if (backtrackStations.isNotEmpty) {
+              chargingStations
+                  .add(backtrackStations.first); // Select the nearest station
+              traveledDistance = 0.0; // Reset distance after a charging stop
+              break;
+            }
+          }
+        }
+      }
+    }
+
+    return chargingStations;
+  }
+
+  Future<List<LatLng>> calculateRoute(LatLng source, LatLng destination) async {
+    final url = 'https://routes.googleapis.com/directions/v2:computeRoutes';
+    final headers = {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': 'AIzaSyD-tezFTBpKRVH1icGYGyJUP4SQPyUrPaE',
+      'X-Goog-FieldMask':
+          'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline',
+    };
+    final body = jsonEncode({
+      'origin': {
+        'location': {
+          'latLng': {
+            'latitude': source.latitude,
+            'longitude': source.longitude,
+          },
+        },
+      },
+      'destination': {
+        'location': {
+          'latLng': {
+            'latitude': destination.latitude,
+            'longitude': destination.longitude,
+          },
+        },
+      },
+      "travelMode": "DRIVE",
+      "routingPreference": "TRAFFIC_AWARE_OPTIMAL",
+      'computeAlternativeRoutes': false,
+      'routeModifiers': {
+        "vehicleInfo": {"emissionType": "ELECTRIC"},
+        'avoidTolls': false,
+        'avoidHighways': false,
+        'avoidFerries': false,
+      },
+      "requestedReferenceRoutes": ["FUEL_EFFICIENT"],
+      'languageCode': 'en-US',
+      'units': 'IMPERIAL',
+    });
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+    );
+
+    final data = json.decode(response.body);
+    final encodedPolyline = data['routes'][0]['polyline']['encodedPolyline'];
+    final List<LatLng> routePoints = decodePolyline(encodedPolyline);
+
+    return routePoints;
+  }
+
+  List<LatLng> decodePolyline(String encoded) {
+    List<LatLng> poly = [];
+    int index = 0, len = encoded.length;
+    int lat = 0, lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.codeUnitAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      poly.add(LatLng(lat / 1E5, lng / 1E5));
+    }
+
+    return poly;
+  }
+
+  double estimateRange(double batteryPercentage, double fullRangeKm) {
+    return (batteryPercentage / 100) * fullRangeKm;
+  }
+}
